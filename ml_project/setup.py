240830eb2c1e11b3f5ba34844148c0123b17bb7f
Azamat Sabyrbayev <azamat.sabyrbayev@btsdigital.kz>
2021-05-10 01:13:12 +0600

    Add requirements to setup.py

--- ml_project/setup.py
+++ ml_project/setup.py
@@ -14,4 +14,14 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.7",
+    install_requires=[
+            'hydra-core==1.0.6',
+            'omegaconf==2.0.6',
+            'pandas==1.2.1',
+            'pandas-profiling==2.12.0',
+            'PyYAML==5.4.1',
+            'scikit-learn==0.23.2',
+            'numpy==1.19.2',
+            'pytest==6.1.1'
+    ]
 )
